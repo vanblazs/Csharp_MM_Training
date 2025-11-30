7907c665f0f9ba8840c58220359b26f59b9a786b
Irvan <Irvan@MYSERVER2>
2025-11-30 20:46:48 +0700

    Add project files.

---
+++ FirstApp/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("My name Budi !");
+            }
+}
